8d9cde41c8d2a6584db8d410b9867364bf5cc202
Maciej Dłuś <trolleyyy@gmail.com>
2020-03-16 21:36:23 +0100

    Better status check

--- bumblebee-status@trolleyyy/applet.js
+++ bumblebee-status@trolleyyy/applet.js
@@ -16,9 +16,9 @@ DEFAULT_BUMBLEBEED_STATUS_TIMEOUT_SECONDS = 2;
 BumblebeeStatusApplet.prototype = {
     __proto__: Applet.TextIconApplet.prototype,
 
-    _init: function(metadata, orientation, panel_height, instance_id) {
+    _init: function (metadata, orientation, panel_height, instance_id) {
         Applet.TextIconApplet.prototype._init.call(this, orientation, panel_height, instance_id);
-        
+
         try {
             this.metadata = metadata;
             Main.systrayManager.registerRole("bumblebee-status", metadata.uuid);
@@ -29,12 +29,12 @@ BumblebeeStatusApplet.prototype = {
         }
     },
 
-    _periodicUpdateIcon: function() {
+    _periodicUpdateIcon: function () {
         this._testStatus();
         return GLib.SOURCE_CONTINUE;
     },
 
-    _restartPeriodicUpdateTimer: function(new_delay) {
+    _restartPeriodicUpdateTimer: function (new_delay) {
         if (this._periodicTimeoutId > 0) {
             Mainloop.source_remove(this._periodicTimeoutId);
         }
@@ -43,49 +43,85 @@ BumblebeeStatusApplet.prototype = {
         this._periodicTimeoutId = Mainloop.timeout_add_seconds(this._updateFrequencySeconds, Lang.bind(this, this._periodicUpdateIcon));
     },
 
-    _testStatus: function() {
-        let srvOut = this._doCommand(["bash", "-c", "ps axco command | grep bumblebeed"])
-        if (srvOut && srvOut.length > 0) {
-            let out = this._doCommand(["cat", "/proc/acpi/bbswitch"]);
-            if (out.endsWith("OFF")) {
-                this.set_applet_icon_name("bumblebee-status-off");
-                this.set_applet_tooltip(_("The high performance GPU is OFF"));
-            } else if (out.endsWith("ON")) {
-                this.set_applet_icon_name("bumblebee-status-on");
-                this.set_applet_tooltip(_("The high performance GPU is ON"));
+    _testStatus: function () {
+        let iconName = "bumblebee-status-unavailable";
+        let statusText = _("The bumblebee is not installed");
+        if (this._isBumblebeeInstalled()) {
+            if (this._isBumblebeeRunning()) {
+                let bbstate = this._getBumblebeeSwitchStatus();
+                if (!bbstate) {
+                    global.logError("bumblebee-status applet: cat /proc/acpi/bbswitch returned unexpected result");
+                    iconName = "bumblebee-status-unavailable";
+                    statusText = _("Test for bumblebee status returned unexpected result");
+                } else {
+                    if (bbstate == "ON") {
+                        iconName = "bumblebee-status-on";
+                        statusText = _("The high performance GPU is ON");
+                    } else {
+                        iconName = "bumblebee-status-off";
+                        statusText = _("The high performance GPU is OFF");
+                    }
+                }
+            } else {
+                iconName = "bumblebee-status-inactive";
+                statusText = _("The bumblebee service is not running")
             }
+        }
+        this.set_applet_icon_name(iconName);
+        this.set_applet_tooltip(statusText);
+    },
+
+    _isBumblebeeInstalled: function () {
+        if (!this._bbInstalled) {
+            let out = this._doCommand(["bash", "-c", "which optirun > /dev/null && which bumblebeed > /dev/null && echo OK"]);
+            this._bbInstalled = out == "OK";
+        }
+        return this._bbInstalled;
+    },
+
+    _isBumblebeeRunning: function () {
+        let out = this._doCommand(["bash", "-c", "ps axco command | grep bumblebeed"]);
+        return out && out.length > 0;
+    },
+
+    _getBumblebeeSwitchStatus: function () {
+        let out = this._doCommand(["cat", "/proc/acpi/bbswitch"]);
+        if (out && out.endsWith("OFF")) {
+            return "OFF";
+        } else if (out && out.endsWith("ON")) {
+            return "ON";
         } else {
-            this.set_applet_icon_name("bumblebee-status-inactive");
-            this.set_applet_tooltip(_("The bumblebee service is not running"));
+            return null;
         }
     },
 
-    _doCommand: function(command) {
-        [res,pid,fdin,fdout,fderr] = GLib.spawn_async_with_pipes(null, command, null, GLib.SpawnFlags.SEARCH_PATH, null);
-        let outstream = new Gio.UnixInputStream({fd:fdout,close_fd:true});
-        let stdout = new Gio.DataInputStream({base_stream: outstream});
+    _doCommand: function (command) {
+        [res, pid, fdin, fdout, fderr] = GLib.spawn_async_with_pipes(null, command, null, GLib.SpawnFlags.SEARCH_PATH, null);
+        let outstream = new Gio.UnixInputStream({ fd: fdout, close_fd: true });
+        let stdout = new Gio.DataInputStream({ base_stream: outstream });
 
         let [out, size] = stdout.read_line(null);
 
         if (out == null) {
-            global.logError("nvidia-prime applet: Could not execute " + command);
+            global.logError("bumblebee-status applet: Could not execute " + command);
+            return null;
         } else {
             return out.toString();
         }
     },
-    
-    on_applet_clicked: function(event) {
+
+    on_applet_clicked: function (event) {
         // this.menu.toggle();
     },
 
-    on_applet_removed_from_panel: function() {
+    on_applet_removed_from_panel: function () {
         Main.systrayManager.unregisterRole("bumblebee-status", this.metadata.uuid);
         if (this._periodicTimeoutId) {
             Mainloop.source_remove(this._periodicTimeoutId);
         }
     },
-    
-    getDisplayLayout: function() {
+
+    getDisplayLayout: function () {
         return Applet.DisplayLayout.BOTH;
     }
 };
